0a4b0aee08170810dee8be3acd4a669dbe69a495
AfkRohan <r_o_h_a_n_99@outlook.com>
2023-09-29 14:36:09 -0400

    Routing added

--- API Server/server.js
+++ API Server/server.js
@@ -2,8 +2,6 @@ const express = require('express')
 
 const cors = require('cors')
 
-const User = require('./models/UserModel')
-
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -20,48 +18,7 @@ app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-// API for creating a new user
-app.post('/create',async (req,res)=>{
-    const data = req.body
-    console.log(data)
-    try{
-    const user = new User({
-    FirstName : data.firstName,
-    LastName : data.lastName,
-    DateOfBirth: data.Date,
-    Password:data.Password,
-    Contact:{
-        email:data.email,
-        phone:data.phone
-    }
-    })
-    const userInserted = await user.save();
-      console.log(userInserted);
-      res.send(userInserted);
-    }
-    catch(err){
-      res.send(`<h1style='color:red;'>${err}</h1>`)
-    }
-})
+const routes = require('./routes/web-config');
 
-// Login API
-app.post('/login',async (req,res)=>{
-    const data = req.body;
-    const userEmail = data.email;
-    console.log(data)
-    const userPassword = data.Password;
-    User.findOne({ 
-    "Contact.email" : userEmail,
-    "Password" : userPassword
-  }).then(
-    (user)=>{
-        console.log(user);
-        res.send(user);
-    }
-  ).catch(
-    (err)=>{
-        console.log(err);
-        res.send(err);
-    }
-  )
-})
+app.use('/api', routes)
+
