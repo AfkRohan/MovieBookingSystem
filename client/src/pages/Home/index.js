cc23283ff767591f8b15a74abeee21ff0e28c748
Ekpreetkaur99 <ekpreetkaur22@gmail.com>
2023-09-29 17:27:57 -0400

    home page created

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import logo from '../../assets/logowhite.png';
 
 function Home(){
     return(
-        <div>Home</div>
+        
+            <div className="bg-primary textxlarge textwhite flex justify-between p2">
+         <img className="m1" src={logo} alt="My Logo" />
+         <div className="p1">
+            <h1 className="textsmall">Home</h1>
+          
+         </div>
+             </div>
+           
        
     )
 }
