77da3819b1553ad75ee43b5f2f0dc8c3296efe30
Ekpreetkaur99 <ekpreetkaur22@gmail.com>
2023-09-29 18:41:03 -0400

    Added Phone Input

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function Footer(){
+    return(
+ <>
+      
+             <div className="bg-primary textxlarge textwhite flex justify-between footersize p2 mt10"></div>
+        </>
+    )
+}
+
+export default Footer
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,24 +1,14 @@
 import React from 'react'
-import logo from '../../assets/logowhite1.png';
-import user from '../../assets/user1.png';
-import search from '../../assets/search1.png'; 
-import ticket from '../../assets/ticket1.png'; 
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home(){
     return(
  <>
-        <div className="bg-primary textxlarge textwhite flex justify-between p2">
-         <img className="m1" src={logo} alt="My Logo" />
-         <div className="p1 flex gap1">
-
-            <input className="m1" type="text" placeholder="Search..."></input>
-              <img className="m1" src={user} alt="user" />
-              <img className="m1" src={ticket} alt="ticket" />
-              <img className="m1" src={search} alt="search" />
-             </div>
-             </div>
+       <Header/>
              <h1 className="m2 tacenter">Welcome to Cineplex Theatre</h1>
-             <div className="bg-primary textxlarge textwhite flex justify-between footersize p2 mt10"></div>
+             <Footer/>
+            
         </>
     )
 }
