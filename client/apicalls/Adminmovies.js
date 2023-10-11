4afb50b510d45586811affa680b35bcae060fc8c
Ekpreetkaur99 <ekpreetkaur22@gmail.com>
2023-10-11 17:58:52 -0400

    AdminMovies

---
+++ client/apicalls/Adminmovies.js
@@ -0,0 +1,14 @@
+const {axiosInstance} = require(".");
+
+//add a new movie
+
+export const AddMovie= async(payload) => {
+    try{
+        const response = await axiosInstance.post("api/movies/add-movie",payload);
+        return response.data;
+    }
+    catch(error){
+        return error.response;
+    }
+
+}
