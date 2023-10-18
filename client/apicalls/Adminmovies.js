4320304bcb1e37c37d53b007fc9452a81a34f746
AfkRohan <r_o_h_a_n_99@outlook.com>
2023-10-18 16:11:36 -0400

    Api call for add movie

--- client/apicalls/Adminmovies.js
+++ client/apicalls/Adminmovies.js
@@ -1,10 +1,10 @@
-const {axiosInstance} = require(".");
+const axiosInstance = require('axios')
 
 //add a new movie
 
 export const AddMovie= async(payload) => {
     try{
-        const response = await axiosInstance.post("api/movies/add-movie",payload);
+        const response = await axiosInstance.post("localhost:4000/api/addmovie",payload);
         return response.data;
     }
     catch(error){
