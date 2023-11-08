cf1d8f6acd33298f58a927aeb504e1b8f49c3761
ronak2012 <radadiyaronak222222@gmail.com>
2023-11-08 14:09:30 -0500

    Movie page Redesigned

--- client/src/pages/AdminMovies/MovieDetails.js
+++ client/src/pages/AdminMovies/MovieDetails.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from 'react-router-dom'; // Import Link
-import { Card, Image, List, Typography, Row, Col, Button } from 'antd'; // Import Button
+import { useParams, Link } from 'react-router-dom';
+import { Card, Image, List, Divider, Tag, Button } from 'antd';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -23,50 +23,51 @@ const MovieDetails = () => {
   }
 
   return (
-    <div className='bootstrap'>
+    <div className="movie-details-container">
       <Header />
-      <Row align="middle" gutter={[16, 16]}>
-        <Col xs={24} md={8} className="text-center" style={{ paddingLeft: '30px' }}>
-          <div style={{ border: '4px solid white', padding: '10px', paddingLeft: '10px' }}>
-            <Image className='imgsize img-fluid' src={movie.image} />
-          </div>
-        </Col>
-        <Col xs={24} md={16}>
-          <Card>
-            <List className='listpadding'>
-              <List.Item className='list-group-item'>
-                <h1 style={{ paddingLeft: '30px' }}>{movie.name}</h1>
+      <div className="movie-background-image">
+        <Image src={movie.image} alt={movie.name} />
+        <div className="movie-overlay" >
+          <Card className="movie-card"  style={{ background: 'transparent' }}>
+            <List className="movie-details-list">
+              <List.Item className="movie-details-title">
+                <h1 className='moviename' style={{ fontSize: '5em' }}>{movie.name}</h1>
               </List.Item>
-              <List.Item className='list-group-item'>
-                <p className='pblack' style={{ paddingLeft: '30px', paddingRight: '30px' }}>{movie.description}</p>
+
+              <List.Item className="movie-details-video">
+            <iframe
+              width="560"
+              height="315"
+              src={movie.trailerLink}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
+          </List.Item>
+
+              <List.Item className="movie-tags" style={{ fontSize: '0.7em' }}>
+                <Tag color="blue" style={{ fontSize: '1.5em' }}>{movie.languages}</Tag>
+                <Tag color="green" style={{ fontSize: '1.5em' }}>{movie.genre}</Tag>
+                <Tag color="orange" style={{ fontSize: '1.5em' }}>Age: {movie.ageGroups}</Tag>
+                <Tag color="purple" style={{ fontSize: '1.5em' }}>Rating: {movie.rating}</Tag>
+                <Tag color="red" style={{ fontSize: '1.5em' }}>Duration: {movie.duration} min</Tag>
               </List.Item>
-              <List.Item className='list-group-item'>
-                <Typography.Text style={{ paddingLeft: '30px' }}>Language:</Typography.Text> {movie.languages}
-              </List.Item>
-              <List.Item className='list-group-item'>
-                <Typography.Text style={{ paddingLeft: '30px' }}>Genre:</Typography.Text> {movie.genre}
-              </List.Item>
-              <List.Item className='list-group-item'>
-                <Typography.Text style={{ paddingLeft: '30px' }}>Age Groups:</Typography.Text> {movie.ageGroups}
-              </List.Item>
-              <List.Item className='list-group-item'>
-                <Typography.Text style={{ paddingLeft: '30px' }}>Rating:</Typography.Text> {movie.rating}
-              </List.Item>
-              <List.Item className='list-group-item'>
-                <Typography.Text style={{paddingLeft:'30px'}}>Duration:</Typography.Text> {movie.duration}
+              <p className='descfont' style={{ fontSize: '1.5em' }}>{movie.description}</p>
+
+
+              <List.Item className="movie-details-seats">
+                <Link to={`/seat-selection/${id}`}>
+                  <Button className="btn-seat-selection">Book Now</Button>
+                </Link>
               </List.Item>
             </List>
-            {/* Add a Button to redirect to the seat selection page */}
-            <Link to={`/seat-selection/${id}`}>
-              <Button className='btnSeatselection'>Select Seats</Button>
-            </Link>
           </Card>
-        </Col>
-      </Row>
+        </div>
+      </div>
       <Footer />
     </div>
   );
 };
-
 
 export default MovieDetails;
