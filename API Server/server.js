5ee52d716fa7c26a95ae9d1df207f930bb09b8b8
parthchodavadiya <parthvisa1717@gmail.com>
2023-11-29 00:54:59 -0500

    favourite and filter

--- API Server/server.js
+++ API Server/server.js
@@ -3,8 +3,10 @@ const express = require('express')
 const cors = require('cors')
 
 const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+
+
 
 const app = express()
 
@@ -26,3 +28,79 @@ const routes = require('./routes/web-config');
 
 app.use('/api', routes)
 
+const url = "mongodb+srv://topa:topachuptha@cluster0.ldhu0gj.mongodb.net/movieApp?retryWrites=true&w=majority";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(console.log("***Mongoose Connected***"))
+  .catch((error) => console.log(error));
+
+
+const movieSchema = new mongoose.Schema({
+  name: String,
+  trailerLink: String,
+  image: String,
+  description: String,
+  languages: String,
+  duration: String,
+  genre: String,
+  rating: Number,
+  ageGroups: String,
+});
+
+
+
+const userSchema = new mongoose.Schema({
+  FirstName: String,
+  LastName: String,
+  DateOfBirth: String,
+  Password: String,
+  Contact: {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    }
+  },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+});
+
+
+
+
+
+
+app.post('/api/register', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.json({ success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.post('/api/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      'Contact.email': req.body.email,
+      'Password': req.body.password
+    });
+
+    if (user) {
+      res.json({ success: true, user });
+    } else {
+      res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
---
+++ client/src/components/FilterMovies.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function FilterMovies({ filterOptions, onFilterChange }) {
+  function handleFilterChange(filter, value){
+    onFilterChange(filter, value);
+  };
+
+
+  return (
+    <div className="filter-options">
+      <label>
+        Rating:
+        <select value={filterOptions.rating || ''} onChange={(e) => handleFilterChange('rating', e.target.value)}>
+          <option value="">All</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+      </label>
+
+      <label>
+        Name:
+        <select value={filterOptions.nameSort} onChange={(e) => handleFilterChange('nameSort', e.target.value)}>
+          <option value="asc">A to Z</option>
+          <option value="desc">Z to A</option>
+        </select>
+      </label>
+
+      <label>
+        Language:
+        <select value={filterOptions.language || ''} onChange={(e) => handleFilterChange('language', e.target.value)}>
+          <option value="">All</option>
+          
+        </select>
+      </label>
+    </div>
+  );
+
+  }
+export default FilterMovies;
